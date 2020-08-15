46914b89d8520ca239a549843bb9fa1dbed9d039
Zekarias Negassa <zakfena@gmail.com>
2020-08-15 11:43:18 -0500

    added api code snippet

--- script.js
+++ script.js
@@ -14,22 +14,22 @@
 //   }).then(function (response) {});
 // }
 
-// $("#searchBtn").on("click", function () {
-//   console.log("button test");
-// });
+$(".searchBtn").on("click", function () {
+  console.log("button test");
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url:
+      "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/anytime?inboundpartialdate=anytime",
+    method: "GET",
+    headers: {
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": "473028eaf3mshe44936fe64e187ep12d58fjsn591246548720",
+    },
+  };
 
-var settings = {
-  async: true,
-  crossDomain: true,
-  url:
-    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-09-01?inboundpartialdate=2019-12-01",
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    "x-rapidapi-key": "473028eaf3mshe44936fe64e187ep12d58fjsn591246548720",
-  },
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 });
